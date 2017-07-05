0d77ec9a5fa9bb2b9eba6ebf5e714c8c0d31ecb8
zhangdaofeng <zhangdf0821@163.com>
2017-07-05 14:56:57 +0800

    use ipcprovider

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var Web3 = require("web3");
+var net = require("net");
 // 创建web3对象
 var web3 = new Web3();
-// 连接到以太坊节点
-web3.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"));
+// 改成你自己的主目录
+var ipcpath = "/home/zhangdaofeng/.ethereum/geth.ipc"
+var socket = new net.Socket();
+web3.setProvider(new Web3.providers.IpcProvider(ipcpath, socket));
 
 // 要生成的账户数量
 var numAccounts = 10;
@@ -10,6 +13,13 @@ var numAccounts = 10;
 var password = "12345";
 
 for (var i = 0; i < numAccounts; i++) {
-    var account = web3.personal.newAccount(password);
-    console.log(account);
+    web3.personal.newAccount(password, function(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(result);
+    });
 }
+
+socket.end();
